9ed34fd2d0afc86a0432dedc3100d95dcabf276e
Yuriy Makov <juramak98@gmail.com>
2017-02-22 13:47:02 +0200

    UPD

--- api_handler.js
+++ api_handler.js
@@ -3,21 +3,39 @@ var mysql= require('mysql');
 var path = require('path');
 
 module.exports={
+    loadItems: function (req, res) {
+
+        // подключение к бд
+        pool.getConnection(function (err, connection) {
+
+            if (err) console.log(err)
+
+            connection.query('SELECT * FROM `users`', function (err, rows) {
+                res.json(rows);
+                connection.release();
+            });
+        });
+
+    },
     createItem: function (req, res) {
 
         // подключение к бд
         pool.getConnection(function (err, connection) {
             if (err) console.log(err)
             var data = req.body;
+            console.log(data.password)
+            console.log(data.userName)
 
-            var sql = 'INSERT INTO `items`(name, description, completed) VALUES (?, ?, ?)';
-            var inserts = [data.name, data.description, data.completed];
+            var sql = 'INSERT INTO `users`(User_name, password) VALUES (?, ?)';
+           /* var inserts = ["Test Item", "Test Description"];*/
+            var inserts = [data.userName, data.password];
             sql = mysql.format(sql, inserts);
 
             connection.query(sql, function (err, rows) {
+                res.send('item updated');
 
                 console.log('item created');
-                res.send('item created');
+
                 connection.release();
             });
         })
--- server.js
+++ server.js
@@ -28,7 +28,29 @@ server.get('/', function (req,res) {
         res.end();
     })
 
+});
+server.get('/users', function (req,res) {
+    fs.readFile(path.join(__dirname,'pages/users.html'),"utf8", function(err, file) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.write(err + "\n");
+            res.end();
+            return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(file);
+        res.end();
+    })
 })
+server.use(restify.bodyParser({ mapParams: true }));
+server.get('/api', apiHandler.loadItems);
+server.post('/api', apiHandler.createItem);
+server.on('InternalServer', function(err) {
+
+    console.log(err)
+    res.send(err);
+});
 
 server.listen(port, function () {
     console.log('server running on port ' + port);
